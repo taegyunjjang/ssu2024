3ebf96071e3150d9d707cba32e230984303ae70e
TGKIM\김태균 <ypp1900@naver.com>
2024-05-19 08:57:31 +0900

    ==== TEST7 ===== unexpected trap 14 from cpu 0 eip 80106715 (cr2=0x80500000) lapicid 0: panic: trap  801051c9 80104f21 80106f70 801072d7 801041e3 8010501d 80104f21 0 0 0QEMU: Terminated

---
+++ OS/slab.c
@@ -0,0 +1,186 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "spinlock.h"
+#include "slab.h"
+
+struct {
+	struct spinlock lock;
+	struct slab slab[NSLAB];
+} stable;
+
+int slab_size[NSLAB] = {16, 32, 64, 128, 256, 512, 1024, 2048};
+
+void slabinit(){
+	initlock(&stable.lock, "slab");
+
+	for (int i = 0; i < NSLAB; i++) {
+		struct  slab *s = &stable.slab[i];
+
+		s->size = slab_size[i];
+		s->num_pages = 1;
+		s->num_free_objects = PGSIZE / s->size;
+		s->num_used_objects = 0;
+		s->num_objects_per_page = PGSIZE / s->size;
+
+		// Allocate one page for bitmap
+		s->bitmap = kalloc();
+		if (s->bitmap == 0) {
+    		panic("slabinit: failed to allocate one page for bitmap");
+		}
+		memset(s->bitmap, 0, s->num_objects_per_page);
+
+		// Allocate one page for slab cache
+		s->page[0] = kalloc();
+		if (s->page[0] == 0) {
+			kfree(s->bitmap);
+			panic("slabinit: failed to allocate one page for slab cache");
+		}
+	}
+}
+
+char *kmalloc(int size){
+	acquire(&stable.lock);
+
+	struct slab *s = 0;
+	
+	// cprintf("prev Kmalloc\n");
+	// slabdump();
+
+	for (int i = 0; i < NSLAB; i++) {
+		if (size <= slab_size[i]) {
+			s = &stable.slab[i];
+			break;
+		}
+	}
+
+	if (s->num_free_objects == 0) {
+		if (s->num_pages >= MAX_PAGES_PER_SLAB) {
+			release(&stable.lock);
+			return 0;
+		}
+
+		char *alloc_page = kalloc();
+		if (alloc_page == 0) {
+			panic("kmalloc: failed to allocate page");
+		}
+
+		int page_idx = s->num_pages;
+		s->page[page_idx] = alloc_page;
+		s->num_pages++;
+		s->num_free_objects += s->num_objects_per_page;
+
+		for (int i = page_idx * s->num_objects_per_page; i < (page_idx + 1) * s->num_objects_per_page; i++) {
+			s->bitmap[i] = 0;
+		}
+	}
+
+	int idx = -1;
+	for (int i = 0; i < s->num_objects_per_page * MAX_PAGES_PER_SLAB; i++) {
+		if (s->bitmap[i] == 0) {
+			idx = i;
+			break;
+		}
+	}
+
+	s->bitmap[idx] = 1;
+	s->num_free_objects--;
+	s->num_used_objects++;
+	
+	int page_idx = idx / s->num_objects_per_page;
+	int offset = idx % s->num_objects_per_page; 
+	char *obj = s->page[page_idx] + offset * s->size;
+
+	release(&stable.lock);
+	
+	// cprintf("next kmalloc\n");
+	// slabdump();
+
+	return obj;
+}
+
+void kmfree(char *addr, int size){
+	acquire(&stable.lock);
+
+	struct slab *s = 0;
+
+	for (int i = 0; i < NSLAB; i++) {
+		if (size <= slab_size[i]) {
+			s = &stable.slab[i];
+			break;
+		}
+	}
+
+	if (s == 0) {
+        release(&stable.lock);
+        panic("kmfree: invalid size");
+    }
+
+	int objects = s->num_objects_per_page;
+
+	int page_idx = -1;
+	for (int i = 0; i < s->num_pages; i++) {
+		if (addr >= s->page[i] && addr < s->page[i] + PGSIZE) {
+			page_idx = i;
+			break;
+		}
+	}
+
+	int offset = (addr - s->page[page_idx]) / s->size;
+
+	int bit_idx =  page_idx * objects + offset;
+	s->bitmap[bit_idx] = 0;
+
+	s->num_free_objects++;
+	s->num_used_objects--;
+	/*
+	// Page deallocation, if all slabs in each page is released
+	int page_dealloc = 1;
+	for (int i = page_idx * objects; i < (page_idx + 1) * objects; i++) {
+		if (s->bitmap[i] == 1) {
+			page_dealloc = 0;
+			break;
+		}
+	}
+
+	if (page_dealloc) {
+		kfree(s->page[page_idx]);
+
+		for (int i = page_idx; i < s->num_pages - 1; i++) {
+			s->page[i] = s->page[i + 1];
+		}
+		s->num_pages--;
+
+		for (int i = page_idx * objects; i < (s->num_pages + 1) * objects; i++) {
+			s->bitmap[i] = s->bitmap[i + objects];
+		}
+	}
+	*/
+	release(&stable.lock);
+}
+
+/* Helper functions */
+void slabdump(){
+	cprintf("__slabdump__\n");
+
+	struct slab *s;
+
+	cprintf("size\tnum_pages\tused_objects\tfree_objects\tbitmap\n");
+
+	for(s = stable.slab; s < &stable.slab[NSLAB]; s++){
+		cprintf("%d\t%d\t\t%d\t\t%d\t\t%d\n", 
+			s->size, s->num_pages, s->num_used_objects, s->num_free_objects, s->bitmap[0]);
+	}
+}
+
+int numobj_slab(int slabid)
+{
+	return stable.slab[slabid].num_used_objects;
+}
+
+int numpage_slab(int slabid)
+{
+	return stable.slab[slabid].num_pages;
+}
